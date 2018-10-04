3b1a2167eb63460e867ab61ef22f0269c0afebb8
shrutishankar <shrutis@geekyants.com>
2018-10-04 17:55:50 +0530

    Add compile script

---
+++ dist/Examples/MakeItRain/index.js
@@ -0,0 +1,2 @@
+var _reactNative=require('react-native');var _App=require('./App');var _App2=_interopRequireDefault(_App);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}_reactNative.AppRegistry.registerComponent('MakeItRain',function(){return _App2.default;});
+//# sourceMappingURL=index.js.map
